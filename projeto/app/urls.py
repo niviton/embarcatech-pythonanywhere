21597b123d406d263893ca0ac11d9afd4be74e5a
niviton <niviton.edu@gmail.com>
2025-03-25 10:20:47 -0300

    Update urls.py

--- projeto/app/urls.py
+++ projeto/app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('api/reservatorio/', views.reservatorio)
+    path('api/reservatorio/', views.reservatorio),
+    path('api/controle-irrigacao/', views.controle_irrigacao),
+    path('api/status-irrigacao/', views.status_irrigacao),
 ]
+
+
+
